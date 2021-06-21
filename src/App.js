9d5046743a3d27628884e84a9e2d9e655adb931f
Giang Do <dhtgiang145@gmail.com>
2021-06-20 20:36:50 -0700

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import theme from "styled-theming";
+import { Provider as ReduxProvider } from "react-redux";
+import store from "./redux/store.js";
+import DarkThemeProvider from "./DarkThemeProvider";
+import DarkThemeToggle from "./DarkThemeToggle";
+import "./App.css";
 
-function App() {
+export const theme1 = theme("theme", {
+  light: "#fff",
+  dark: "#2d2d2d",
+});
+
+export const theme2 = theme("theme", {
+  light: "#000",
+  dark: "#fff",
+});
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100 vw;
+  height: 100 vh,
+  align-items: center;
+  justify-content: center;
+  font-family: sans-serif;
+  background-color: ${theme1};
+  `;
+const SwitchTheme = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 50px;
+  height: 75px;
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <ReduxProvider store={store}>
+        <DarkThemeProvider>
+          <SwitchTheme>
+            <h1>Theme App</h1>
+            <p>
+              <DarkThemeToggle />
+            </p>
+          </SwitchTheme>
+          <Container>
+            <h2>Welcome</h2>
+            <h3>Full Stack Web Development</h3>
+          </Container>
+        </DarkThemeProvider>
+      </ReduxProvider>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
